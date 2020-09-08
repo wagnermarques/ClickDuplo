3c0213104aa20c2cece3b7b67df2bbc21a6431bd
wagnermarques <wagnerdocri@gmail.com>
2020-09-07 23:55:18 -0400

    trying to deploy on heroku

--- clickduplo/server.js
+++ clickduplo/server.js
@@ -3,8 +3,11 @@ const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 
 const app = express();
+const path = require('path');
 const config = require('./webpack.config.js');
 const compiler = webpack(config);
+
+const port = process.env.PORT || 3000;
 
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
 // configuration file as a base.
@@ -12,7 +15,14 @@ app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath,
 }));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './dist/index.html'));
+});
+
+app.use(express.static(__dirname + '/dist'));
+
 // Serve the files on port 3000.
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!\n');
 });
+
